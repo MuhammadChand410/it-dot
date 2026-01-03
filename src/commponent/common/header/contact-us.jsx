d9713254f0bd5bd0aad327fb7451b691a7688326
MuhammadChand410 <muhammadchand00001@gmail.com>
2026-01-03 10:47:00 +0500

    add contact us button

--- src/commponent/common/header/contact-us.jsx
+++ src/commponent/common/header/contact-us.jsx
@@ -1,7 +1,8 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import Button from "./button";
-import { ContactUsImage } from "../../../assets/images"; 
+import { ContactUsImage } from "../../../assets/images";
+import { ArrowIcon } from "../../../assets/icon";
 
 export default function ComponentContactUs({ heading, description }) {
     const validationSchema = Yup.object({
@@ -90,7 +91,11 @@ export default function ComponentContactUs({ heading, description }) {
                                             className="text-sm text-red-500 pt-1"
                                         />
                                     </div>
-                                    <Button text={`Send Message`} />
+                                    {/* <Button text={`Send Message`} /> */}
+                                    <button type="submint" className={`bg-[#0160C9] shadow-xl flex w-fit cursor-pointer py-2 items-center gap-3 px-4 rounded-full text-white text-base font-semibold hover:bg-[#014fa8] transition-all duration-200`}>
+                                        Send Message
+                                        <ArrowIcon />
+                                    </button>
                                 </Form>
                             )}
                         </Formik>
